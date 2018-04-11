7d05aeb00f75e6599ddfa0e959909b24265517f9
Stephane Epardaud <stef@epardaud.fr>
2018-04-11 12:23:13 +0200

    Added stuff to my chapter

---
+++ src/main/java/me/escoffier/lab/chapter5/Code01.java
@@ -0,0 +1,19 @@
+package me.escoffier.lab.chapter5;
+
+import io.reactivex.Single;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.file.FileSystem;
+
+public class Code01 {
+
+	static Single<String> getHeroesFile() {
+		Vertx vertx = Vertx.vertx();
+		FileSystem fileSystem = vertx.fileSystem();
+		return fileSystem.rxReadFile("src/main/resources/entities.json")
+				.map(buffer -> buffer.toString());
+	}
+
+    public static void main(String[] args) {
+    	getHeroesFile().subscribe(System.out::println, Throwable::printStackTrace);
+    }
+}
---
+++ src/main/java/me/escoffier/lab/chapter5/Code03_Solution.java
@@ -0,0 +1,30 @@
+package me.escoffier.lab.chapter5;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.core.file.FileSystem;
+import me.escoffier.superheroes.SuperStuff;
+
+public class Code03_Solution {
+
+	static Flowable<SuperStuff> getHeroesFromFile(Single<String> file) {
+		Vertx vertx = Vertx.vertx();
+		FileSystem fileSystem = vertx.fileSystem();
+		return file.flatMap(name -> fileSystem.rxReadFile(name))
+                .map(Buffer::toJsonArray)
+                .flatMapPublisher(Flowable::fromIterable)
+                .cast(JsonObject.class)
+                .map(j -> j.mapTo(SuperStuff.class))
+                .filter(s -> ! s.isVillain());
+	}
+	
+	
+
+    public static void main(String[] args) {
+    	Single<String> file = Single.just("src/main/resources/entities.json");
+    	getHeroesFromFile(file).subscribe(System.out::println, Throwable::printStackTrace);
+    }
+}
